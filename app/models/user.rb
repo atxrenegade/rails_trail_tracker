dcb26d0ebdb93c800c16bd80a30aacfc0319d16e
atxrenegade <harleighabel@gmail.com>
2018-12-30 23:38:14 -0600

    debug users sign up and validation errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,19 @@ class User < ApplicationRecord
 	has_many :hikes
 	has_many :trails, through: :hikes
 
-	validates :name, presence: true, message: "Username cannot be blank"
-	validates :name, uniqueness: true, message: "This username has already been taken."
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "Enter a valid email address" }
-	validates :email, uniqueness: true, message: "This email address is already associated with an existing account. Please sign in."
-	validates :password, presence: true, message: "Password cannot be empty"
-	validates :password,  confirmation: true, message: "Passwords must match."
+	validates :name, {
+		presence: { message: "Username cannot be blank"},
+		uniqueness: { message: "This username has already been taken." }
+	}
+	validates :email, {
+		format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,  message: "Enter a valid email address" },
+		presence: { message: "Email address cannot be blank"},
+		uniqueness: { message: "This email address is already associated with an existing account. Please sign in." }
+	}
+
+	validates :password, {
+		presence: { message: "Email address cannot be blank"},
+		uniqueness: { message: "This email address is already associated with an existing account. Please sign in." },
+		confirmation: { message: "Passwords must match."}
+	}
 end
