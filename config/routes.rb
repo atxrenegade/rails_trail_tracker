6d237431d20e0b9d259f4aa1b4ae3add802affef
atxrenegade <harleighabel@gmail.com>
2018-12-30 19:41:47 -0600

    add routes for signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   resources :hikes
   resources :trails
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
