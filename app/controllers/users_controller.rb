d4200df37e895e4b51c0d50dd3cfbee64a901ada
atxrenegade <harleighabel@gmail.com>
2018-12-30 21:18:10 -0600

    debug users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 	def new
-		@user = User.new
+		user = User.new
 		render :sign_up
 	end
 
 	def create
-		@user = User.create(user_params)
-		if @user
-			session[:id] = @user[:id]
+		user = User.new(user_params)
+		if user.valid?
+			user.save
+			session[:id] = user[:id]
 			redirect_to '/trails/index'
 		else
 			render :sign_up
