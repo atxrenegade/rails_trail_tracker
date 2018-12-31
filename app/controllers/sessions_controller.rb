71fbe2c17e23bb2037df186a83ddf0133cc4d1a2
atxrenegade <harleighabel@gmail.com>
2018-12-30 20:13:21 -0600

    add Session actions new and create, debug login view page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+	def new
+		@session = Session.new
+		render :login
+	end
+
+	def create
+		@session = Session.create
+		redirect_to '/users/show'
+	end
 end
