0c2f39beedfe1a2d9418868b626131d8f50d4dde
atxrenegade <harleighabel@gmail.com>
2018-12-30 18:49:10 -0600

    correct hikes table data types

--- db/migrate/20181115044838_create_hikes.rb
+++ db/migrate/20181115044838_create_hikes.rb
@@ -3,8 +3,8 @@ class CreateHikes < ActiveRecord::Migration[5.2]
     create_table :hikes do |t|
       t.string :date_of_hike
       t.text :comments
-	  t.user_id
-	  t.trail_id
+	  t.integer :user_id
+	  t.integer :trail_id
       t.timestamps
     end
   end
