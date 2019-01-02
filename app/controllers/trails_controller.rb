cc44b7cd5fa4abea406045174fde0913369c07d0
atxrenegade <harleighabel@gmail.com>
2019-01-02 00:02:30 -0600

    add new create and show controller actions to TrailsController

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,2 +1,20 @@
 class TrailsController < ApplicationController
+	def show
+		@trail = Trail.find_by(id: params[:id])
+		render :show
+	end
+
+	def new
+		
+	end
+
+	def create
+		@trail = Trail.create(trail_params)
+		redirect_to trail_path
+	end
+
+	private
+	def trail_params
+		params.require(:trail).permit(:name, :start_point, :end_point, :difficulty, :length_of_hike, :terrain, :notes)
+	end
 end
