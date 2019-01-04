bac44ed5a17211738c4c98b0d5645ed067f87463
atxrenegade <harleighabel@gmail.com>
2019-01-03 23:34:17 -0600

    debug trail create action

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -12,7 +12,8 @@ class HikesController < ApplicationController
 	end
 
 	def create
-
+		@hike = Hike.create(hike_params)
+		redirect_to @hike
 	end
 
 	def edit
@@ -24,6 +25,10 @@ class HikesController < ApplicationController
 
 	private
 	def trails_params
-
+		params.require(:hike).permit(:date, :comments)
 	end
 end
+
+#create nested resource routes for hikes under user show page
+#implement join table for hikes user_id and hikes trail_id
+#add drop down menu for trail id
