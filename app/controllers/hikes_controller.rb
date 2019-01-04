03fab11ea91ca324ff5ac72636c0b751e32c68f5
atxrenegade <harleighabel@gmail.com>
2019-01-03 23:26:21 -0600

    add actions to Hikes Controller

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,29 @@
 class HikesController < ApplicationController
+	def index
+		@trails = Trail.all
+	end
+
+	def show
+		@trail = Trail.find_by(id: params[:id])
+	end
+
+	def new
+		@trail = Trail.new
+	end
+
+	def create
+
+	end
+
+	def edit
+		@trail = Trail.find_by(id: params[id])
+	end
+
+	def update
+	end
+
+	private
+	def trails_params
+
+	end
 end
