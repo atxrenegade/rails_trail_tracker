0a07b71eb4051d846aacd34c2e8cddd54a70ee5b
atxrenegade <harleighabel@gmail.com>
2019-04-14 00:13:39 -0500

    add root route directed to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :trails
   resources :users, only: [:show]
 
+	get '/', to:'sessions#new'
+
   get '/sign_up', to: 'users#new'
   post '/sign_up', to: 'users#create'
 
