0d1af35884482e604294f84bd7824a344f315173
atxrenegade <harleighabel@gmail.com>
2019-04-17 19:53:43 -0500

    add date format validation for hikes model and form

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -2,4 +2,8 @@ class Hike < ApplicationRecord
 	belongs_to :user
 	belongs_to :trail
 	has_many :pictures, as: :imageable
+
+	validates :date_of_hike,
+		format: { with: /\d{2}\/\d{2}\/\d{2}/, message: "Please Enter Valid Date Format" }
+		
 end
