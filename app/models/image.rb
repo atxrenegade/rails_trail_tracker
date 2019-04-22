daa2f350a0488402545de139c2e8aa11179525d3
atxrenegade <harleighabel@gmail.com>
2019-04-22 02:24:29 -0500

    add validations to new images page

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,4 +4,13 @@ class Image < ApplicationRecord
 	def public_images
 		Image.where(is_public: true)
 	end
+
+	validates :date,
+		format: { with: /\d{2}\/\d{2}\/\d{2}/, message: " - Please Enter Valid Date Format" },
+		presence: true
+
+	validates :url,
+		presence: true,
+		format: { with: // , message: " - Please Enter a Valid Web Address" }
+
 end
