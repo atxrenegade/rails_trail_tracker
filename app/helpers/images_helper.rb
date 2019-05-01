242f830ad5966094c67159620ff5a250071e58fa
atxrenegade <harleighabel@gmail.com>
2019-05-01 00:48:37 -0500

    debug trails show page gallery

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -43,10 +43,15 @@ module ImagesHelper
 	end
 
 	def collect_user_trail_images
-		trail_id = @trail.id
-		@images = Image.where(trail_id: trail_id)
-		@images = Image.where(username: current_user.name).or(Image.where(is_public: true))
+		trail_name = @trail.name
+		@images = Image.where(trail_name: trail_name).where(username: current_user.name)
 	end
+
+	def collect_public_trail_images
+		trail_name = @trail.name
+		@images = Image.where(trail_name: trail_name).where(is_public: true)
+	end
+
 
 	def collect_public_images
 		@images = Image.where(is_public: true)
