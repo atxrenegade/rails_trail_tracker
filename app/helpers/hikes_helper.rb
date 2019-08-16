a5b09e0c92c55f33bccd6e3886154118b0001ad7
atxrenegade <harleighabel@gmail.com>
2019-08-16 01:48:58 -0500

    remove redundant line in HikesHelper build_hikes_params

--- app/helpers/hikes_helper.rb
+++ app/helpers/hikes_helper.rb
@@ -1,13 +1,12 @@
 module HikesHelper
 	def build_hike_params(params)
-		user = current_user.id
 	  params["hike"]["is_public"].to_i == 1 ? is_public = true : is_public = false
 		#can this be simplified using an exisitng helper or method?
 
 		@hike_params = {"date_of_hike": params["hike"]["date_of_hike"],
  		"trail_id": params["hike"]["trail_id"],
  		"comments": params["hike"]["comments"],
- 		"is_public": is_public, "user_id": user}
+ 		"is_public": is_public, "user_id": current_user.id}
 	end
 
 	def public_user(hike)
