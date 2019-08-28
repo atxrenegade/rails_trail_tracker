5820dc9f2d61b789665743013af09c90fac35f74
atxrenegade <harleighabel@gmail.com>
2019-08-27 22:17:12 -0500

    debug session login flow for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			session[:id] = @user[:id]
+			session[:user_id] = @user[:id]
 			redirect_to '/welcome'
 		else
 			render :signup
